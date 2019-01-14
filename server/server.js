4f2569c720a432020a0d4d4b1d2e366d9cec63d0
gordon <user@Users-MBP.Dlink>
2019-01-14 15:07:07 +0200

    initial commit of chat app

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server up on port 3000!');
+});
+

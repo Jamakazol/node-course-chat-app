713452dd0b86822f3a36c71876c851c0cd11188f
Gordon <gordon.henning@gmail.com>
2019-01-24 09:46:45 +0200

    latest

--- server/server.js
+++ server/server.js
@@ -2,16 +2,38 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); 
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.emit('newMessage', {
+        from: 'gordon.henning@gmail.com',
+        text: 'Hey, whasup',
+        createdAt: '123time'
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('create message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server up on port ${port}`);
 });
 
